4f2f81153d3e94abc3bef3df65d8ab08cf2bd938
sassas <aaa@aaa.aaa>
2018-10-10 08:40:24 +0300

    Initial commit

---
+++ app/src/main/java/noran/myfinalproject2018/NoranActivity12.java
@@ -0,0 +1,13 @@
+package noran.myfinalproject2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoranActivity12 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noran12);
+    }
+}
